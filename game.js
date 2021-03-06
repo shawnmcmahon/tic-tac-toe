72915d1ab51111d55e013c443ec5c733a24d82cc
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-06 12:55:00 -0700

    Progress on Game data model logic

--- game.js
+++ game.js
@@ -1,59 +1,91 @@
 class Game {
   constructor(details) {
-    this.heartTurn = details.heartTurn;
-    this.starWins = details.starWins;
-    this.heartWins = details.heartWins || 0;
-    this.starWins = details.starWins || 0;
+    this.heartTurn = details.heartTurn || true;
+    this.starTurn = details.starTurn || false;
+    this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
     this.heartMoves = details.heartMoves || [];
     this.starMoves = details.starMoves || [];
 
   }
 
-  switchTurn() {
+  playTurn(square) {
+    if (this.heartTurn) {
+      for (var i = 0; i < this.gameArray.length; i++) {
+        if (this.gameArray.includes(square)) {
+          var playersChoosenSqaure = this.gameArray.splice(i , 1);
+          this.heartMoves.push(playersChoosenSqaure);
+          console.log('Heart squares are', this.heartMoves)
+          this.heartTurn = false;
+          this.StarTurn = true;
+        } else {
+          return "Pick an available square Heart Player";
+        }
+      }
+    } else if (this.StarTurn) {
+        for (var i = 0; i < this.gameArray.length; i++) {
+          if (this.gameArray.includes(square)) {
+            var playersChoosenSqaure = this.gameArray.splice(i , 1);
+            this.starMoves.push(playersChoosenSqaure);
+            console.log('Star squares are', this.starMoves)
+            this.starTurn = false;
+            this.heartTurn = true;
+        } else {
+          return "Pick an available square Star Player";
+        }
+      }
+
+
+
+    }
+
 
   }
 
   declareWinner() {
-    if(heartMoves.includes("a1") && heartMoves.includes("a2") && heartMoves.includes("a3") ||
-       heartMoves.includes("b1") && heartMoves.includes("b2") && heartMoves.includes("b3") ||
-       heartMoves.includes("c1") && heartMoves.includes("c2") && heartMoves.includes("c3") ||
-       heartMoves.includes("a1") && heartMoves.includes("b1") && heartMoves.includes("c1") ||
-       heartMoves.includes("a2") && heartMoves.includes("b2") && heartMoves.includes("c2") ||
-       heartMoves.includes("a3") && heartMoves.includes("b3") && heartMoves.includes("c3") ||
-       heartMoves.includes("a1") && heartMoves.includes("b2") && heartMoves.includes("c3") ||
-       heartMoves.includes("c1") && heartMoves.includes("b2") && heartMoves.includes("a3") {
-         this.heartWins++;
+    if(this.heartMoves.includes("a1") && this.heartMoves.includes("a2") && this.heartMoves.includes("a3") ||
+       this.heartMoves.includes("b1") && this.heartMoves.includes("b2") && this.heartMoves.includes("b3") ||
+       this.heartMoves.includes("c1") && this.heartMoves.includes("c2") && this.heartMoves.includes("c3") ||
+       this.heartMoves.includes("a1") && this.heartMoves.includes("b1") && this.heartMoves.includes("c1") ||
+       this.heartMoves.includes("a2") && this.heartMoves.includes("b2") && this.heartMoves.includes("c2") ||
+       this.heartMoves.includes("a3") && this.heartMoves.includes("b3") && this.heartMoves.includes("c3") ||
+       this.heartMoves.includes("a1") && this.heartMoves.includes("b2") && this.heartMoves.includes("c3") ||
+       this.heartMoves.includes("c1") && this.heartMoves.includes("b2") && this.heartMoves.includes("a3") {
+         playerHeart.wins++;
+         console.log('Heart player wins!');
          //(need to send message to h1 that heart wins)
-       } else if (starMoves.includes("a1") && starMoves.includes("a2") && starMoves.includes("a3") ||
-          starMoves.includes("b1") && starMoves.includes("b2") && starMoves.includes("b3") ||
-          starMoves.includes("c1") && starMoves.includes("c2") && starMoves.includes("c3") ||
-          starMoves.includes("a1") && starMoves.includes("b1") && starMoves.includes("c1") ||
-          starMoves.includes("a2") && starMoves.includes("b2") && starMoves.includes("c2") ||
-          starMoves.includes("a3") && starMoves.includes("b3") && starMoves.includes("c3") ||
-          starMoves.includes("a1") && starMoves.includes("b2") && starMoves.includes("c3") ||
-          starMoves.includes("c1") && starMoves.includes("b2") && starMoves.includes("a3") {
-            this.starWins++;
+         resetGame();
+       } else if (this.starMoves.includes("a1") && this.starMoves.includes("a2") && this.starMoves.includes("a3") ||
+          this.starMoves.includes("b1") && this.starMoves.includes("b2") && this.starMoves.includes("b3") ||
+          this.starMoves.includes("c1") && this.starMoves.includes("c2") && this.starMoves.includes("c3") ||
+          this.starMoves.includes("a1") && this.starMoves.includes("b1") && this.starMoves.includes("c1") ||
+          this.starMoves.includes("a2") && this.starMoves.includes("b2") && this.starMoves.includes("c2") ||
+          this.starMoves.includes("a3") && this.starMoves.includes("b3") && this.starMoves.includes("c3") ||
+          this.starMoves.includes("a1") && this.starMoves.includes("b2") && this.starMoves.includes("c3") ||
+          this.starMoves.includes("c1") && this.starMoves.includes("b2") && this.starMoves.includes("a3") {
+            playerStar.wins++;
+            console.log('Star player wins');
             //(need to send message to h1 that star wins)
+            resetGame();
+          } else if(this.gameArray === []) {
+            console.log('The game is a tie!')
+            resetGame();
           }
   }
 
   resetGame() {
     this.heartMoves = [];
     this.starMoves = [];
-
-    //Need to reset the startofGameArray
-
+    this.GameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
 
   }
 
 
 }
 
-var playerHeart = new Player({"heart","heart-img"});
+var playerHeart = new Player({playerID: "heart", token: "❤️"});
 console.log(playerHeart);
-var playerStar = new Player({"star","star-img"});
+var playerStar = new Player({playerID: "star", token: "⭐️"});
 console.log(playerStart);
-
 
 
 
--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 //querySelectors
-var squareA1 = document.querySelector('#a1');
-var squareA2 = document.querySelector('#a2');
-var squareA3 = document.querySelector('#a3');
-var squareB1 = document.querySelector('#b1');
-var squareB2 = document.querySelector('#b2');
-var squareB3 = document.querySelector('#b3');
-var squareC1 = document.querySelector('#c1');
-var squareC2 = document.querySelector('#c2');
-var squareC3 = document.querySelector('#c3');
+var a1 = document.querySelector('#a1');
+var a2 = document.querySelector('#a2');
+var a3 = document.querySelector('#a3');
+var b1 = document.querySelector('#b1');
+var b2 = document.querySelector('#b2');
+var b3 = document.querySelector('#b3');
+var c1 = document.querySelector('#c1');
+var c2 = document.querySelector('#c2');
+var c3 = document.querySelector('#c3');
 
 //arrays
-var startofGameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+
 
 
 //eventListeners
-squareA1.addEventListener('click', playerTakeSquare);
-squareA2.addEventListener('click', playerTakeSquare);
-squareA3.addEventListener('click', playerTakeSquare);
-squareB1.addEventListener('click', playerTakeSquare);
-squareB2.addEventListener('click', playerTakeSquare);
-squareB3.addEventListener('click', playerTakeSquare);
-squareC1.addEventListener('click', playerTakeSquare);
-squareC2.addEventListener('click', playerTakeSquare);
-squareC3.addEventListener('click', playerTakeSquare);
+a1.addEventListener('click', playerTakeSquare);
+a2.addEventListener('click', playerTakeSquare);
+a3.addEventListener('click', playerTakeSquare);
+b1.addEventListener('click', playerTakeSquare);
+b2.addEventListener('click', playerTakeSquare);
+b3.addEventListener('click', playerTakeSquare);
+c1.addEventListener('click', playerTakeSquare);
+c2.addEventListener('click', playerTakeSquare);
+c3.addEventListener('click', playerTakeSquare);
 
 
 //functions
-function playerTakeSquare(squareLocation) {
+  //Take the correct squareLocation from the startofGameArray and
+
+  //push to the correct players array.
+
 
 }
