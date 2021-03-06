b75dc7dfcc88ca72e9179b79614070242ac9d196
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-05 20:18:29 -0700

    Add querySelectors

---
+++ main.js
@@ -0,0 +1,15 @@
+//querySelectors
+var squareA1 = document.querySelector('#a1');
+var squareA2 = document.querySelector('#a2');
+var squareA3 = document.querySelector('#a3');
+var squareB1 = document.querySelector('#b1');
+var squareB2 = document.querySelector('#b2');
+var squareB3 = document.querySelector('#b3');
+var squareC1 = document.querySelector('#c1');
+var squareC2 = document.querySelector('#c2');
+var squareC3 = document.querySelector('#c3');
+
+//eventListeners
+
+
+//functions
