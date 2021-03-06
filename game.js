881210b1deccdedd3ceddfa861958be7cfc339ef
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-06 11:16:05 -0700

    Start declare winner function

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ class Game {
     this.heartWins = details.heartWins || 0;
     this.starWins = details.starWins || 0;
     this.heartMoves = details.heartMoves || [];
-    this.starMoves = details.starWins || [];
+    this.starMoves = details.starMoves || [];
 
   }
 
@@ -14,13 +14,36 @@ class Game {
   }
 
   declareWinner() {
-
-
-    //if condition to clear draw
+    if(heartMoves.includes("a1") && heartMoves.includes("a2") && heartMoves.includes("a3") ||
+       heartMoves.includes("b1") && heartMoves.includes("b2") && heartMoves.includes("b3") ||
+       heartMoves.includes("c1") && heartMoves.includes("c2") && heartMoves.includes("c3") ||
+       heartMoves.includes("a1") && heartMoves.includes("b1") && heartMoves.includes("c1") ||
+       heartMoves.includes("a2") && heartMoves.includes("b2") && heartMoves.includes("c2") ||
+       heartMoves.includes("a3") && heartMoves.includes("b3") && heartMoves.includes("c3") ||
+       heartMoves.includes("a1") && heartMoves.includes("b2") && heartMoves.includes("c3") ||
+       heartMoves.includes("c1") && heartMoves.includes("b2") && heartMoves.includes("a3") {
+         this.heartWins++;
+         //(need to send message to h1 that heart wins)
+       } else if (starMoves.includes("a1") && starMoves.includes("a2") && starMoves.includes("a3") ||
+          starMoves.includes("b1") && starMoves.includes("b2") && starMoves.includes("b3") ||
+          starMoves.includes("c1") && starMoves.includes("c2") && starMoves.includes("c3") ||
+          starMoves.includes("a1") && starMoves.includes("b1") && starMoves.includes("c1") ||
+          starMoves.includes("a2") && starMoves.includes("b2") && starMoves.includes("c2") ||
+          starMoves.includes("a3") && starMoves.includes("b3") && starMoves.includes("c3") ||
+          starMoves.includes("a1") && starMoves.includes("b2") && starMoves.includes("c3") ||
+          starMoves.includes("c1") && starMoves.includes("b2") && starMoves.includes("a3") {
+            this.starWins++;
+            //(need to send message to h1 that star wins)
+          }
   }
 
   resetGame() {
-    
+    this.heartMoves = [];
+    this.starMoves = [];
+
+    //Need to reset the startofGameArray
+
+
   }
 
 
@@ -28,7 +51,7 @@ class Game {
 
 var playerHeart = new Player({"heart","heart-img"});
 console.log(playerHeart);
-var playerStart = new Player({"start","star-img"});
+var playerStar = new Player({"star","star-img"});
 console.log(playerStart);
 
 
--- main.js
+++ main.js
@@ -9,7 +9,23 @@ var squareC1 = document.querySelector('#c1');
 var squareC2 = document.querySelector('#c2');
 var squareC3 = document.querySelector('#c3');
 
+//arrays
+var startofGameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+
+
 //eventListeners
+squareA1.addEventListener('click', playerTakeSquare);
+squareA2.addEventListener('click', playerTakeSquare);
+squareA3.addEventListener('click', playerTakeSquare);
+squareB1.addEventListener('click', playerTakeSquare);
+squareB2.addEventListener('click', playerTakeSquare);
+squareB3.addEventListener('click', playerTakeSquare);
+squareC1.addEventListener('click', playerTakeSquare);
+squareC2.addEventListener('click', playerTakeSquare);
+squareC3.addEventListener('click', playerTakeSquare);
 
 
 //functions
+function playerTakeSquare(squareLocation) {
+
+}
