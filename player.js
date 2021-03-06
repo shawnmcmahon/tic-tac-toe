8d1e7c6998bbfc720611af8fd3f441c4f0a6b7b7
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-05 19:10:42 -0700

    Add player constructor and properties

---
+++ player.js
@@ -0,0 +1,17 @@
+class Player {
+  constructor(detail) {
+    this.playerID = details.playerID;
+    this.token = details.token;
+    this.wins = details.wins;
+  }
+
+  saveWinstoStore() {
+
+  }
+
+  retrieveWinsFromStorage() {
+    
+  }
+}
+
+exports.module = Player;
