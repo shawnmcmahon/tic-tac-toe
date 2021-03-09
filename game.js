595dda8a1810d7b04866ce2aa2e83212babfb85b
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-09 15:42:27 -0700

    Start clear game and some css

--- game.js
+++ game.js
@@ -7,6 +7,8 @@ class Game {
     this.starMoves = [];
     this.playerHeart = new Player({playerID: "heart", token: 	"hhh", wins: 0});
     this.playerStar = new Player({playerID: "star", token: "sss", wins: 0});
+    this.heartWinDeclared = false;
+    this.starWinDeclared = false;
 
   }
 
@@ -26,13 +28,15 @@ class Game {
          this.playerHeart.wins++;
          this.playerHeart.saveWinsToStorage();
          console.log('Heart player wins!');
+         this.heartWinDeclared = true;
          //all dom manipulation has to be inside main.js
-         heartScore.innerHTML = `Heart player has ${this.playerHeart.wins} wins`;
-         referee.innerHTML = `${this.playerHeart.token} won!`;
+         //heartScore.innerHTML = `Heart player has ${this.playerHeart.wins} wins`;
+         //referee.innerHTML = `${this.playerHeart.token} won!`;
          //reset game
-         this.heartMoves = [];
-         this.starMoves = [];
-         this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+         //this.heartMoves = [];
+         //this.starMoves = [];
+         //this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
+         //this.heartWinDeclared = false;
        } else if (this.starMoves.includes("a1") && this.starMoves.includes("a2") && this.starMoves.includes("a3") ||
           this.starMoves.includes("b1") && this.starMoves.includes("b2") && this.starMoves.includes("b3") ||
           this.starMoves.includes("c1") && this.starMoves.includes("c2") && this.starMoves.includes("c3") ||
@@ -44,18 +48,20 @@ class Game {
             this.playerStar.wins++;
             this.playerStar.saveWinsToStorage();
             console.log('Star player wins');
+            this.starWinDeclared = true;
             //all dom manipulation has to be inside main.js
-            referee.innerHTML = `${this.playerStar.token} won!`;
-            starScore.innerHTML = `Star player has ${this.playerStar.wins} wins`;
+            //referee.innerHTML = `${this.playerStar.token} won!`;
+            //starScore.innerHTML = `Star player has ${this.playerStar.wins} wins`;
             //reset game
-            this.heartMoves = [];
-            this.starMoves = [];
-            this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+            //this.heartMoves = [];
+            //this.starMoves = [];
+            //this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+            //this.heartWinDeclared = false;
           } else if(this.gameArray.length === 0) {
             console.log('The game is a tie!')
-            this.heartMoves = [];
-            this.starMoves = [];
-            this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+            //this.heartMoves = [];
+            //this.starMoves = [];
+            //this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
           }
   }
 }
@@ -64,5 +70,7 @@ class Game {
     this.heartMoves = [];
     this.starMoves = [];
     this.gameArray = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+    this.heartWinDeclared = false;
+    this.starWinDeclared = false;
 
   }
