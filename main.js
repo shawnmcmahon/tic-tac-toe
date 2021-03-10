c292558e36b44c13cfd65d247d452eb860eb3c5e
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-10 11:17:30 -0700

    js formatting

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//querySelectors
 var a1 = document.querySelector('#a1');
 var a2 = document.querySelector('#a2');
 var a3 = document.querySelector('#a3');
@@ -17,11 +16,10 @@ var newGame = new Game({heartTurn: true, starTurn: false,
                         gameArray: ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'],
                         heartMoves:[], starMoves:[]});
 
-//eventListeners
+
 gameBoard.addEventListener('click', playerTakeSquare);
 window.addEventListener('load', retrieveWins);
 
-//functions
 function playerTakeSquare(event, square) {
   var square = event.target.id;
   renderGameBoard();
@@ -34,7 +32,7 @@ function playerTakeSquare(event, square) {
     newGame.checkForWinner();
     newGame.heartTurn = false;
     newGame.starTurn = true;
-  } else if(newGame.starTurn && newGame.gameArray.includes(square)) {
+  } else if (newGame.starTurn && newGame.gameArray.includes(square)) {
     var selectedSquareIndex = newGame.gameArray.indexOf(square);
     newGame.gameArray.splice(selectedSquareIndex, 1);
     newGame.starMoves.push(square);
@@ -60,58 +58,56 @@ function retrieveWins() {
   heartScore.innerText = `${newGame.playerHeart.wins} wins`;
   starScore.innerText = `${newGame.playerStar.wins} wins`;
 
-  if(newGame.playerHeart.wins === null) {
-    heartScore.innerText = `0 wins`;
+  if (newGame.playerHeart.wins === null) {
+    heartScore.innerText = '0 wins';
   }
+
   if (newGame.playerStar.wins === null) {
-    starScore.innerText = `0 wins`;
+    starScore.innerText = '0 wins';
   }
  }
 
  function renderGameBoard() {
-   checkSpaceValue(a1, "a1");
-   checkSpaceValue(a2, "a2");
-   checkSpaceValue(a3, "a3");
-   checkSpaceValue(b1, "b1");
-   checkSpaceValue(b2, "b2");
-   checkSpaceValue(b3, "b3");
-   checkSpaceValue(c1, "c1");
-   checkSpaceValue(c2, "c2");
-   checkSpaceValue(c3, "c3");
+   checkSpaceValue(a1, 'a1');
+   checkSpaceValue(a2, 'a2');
+   checkSpaceValue(a3, 'a3');
+   checkSpaceValue(b1, 'b1');
+   checkSpaceValue(b2, 'b2');
+   checkSpaceValue(b3, 'b3');
+   checkSpaceValue(c1, 'c1');
+   checkSpaceValue(c2, 'c2');
+   checkSpaceValue(c3, 'c3');
  }
 
- function checkSpaceValue(space, id) {
-   space.innerText = newGame.playerStar.token;
-   if (newGame.gameArray.includes(id)) {
-     space.innerText = "";
-   } else if (newGame.heartMoves.includes(id)) {
-     space.innerText = newGame.playerHeart.token;
-   } else if (newGame.starMoves.includes(id)) {
-     space.innerText = newGame.playerStar.token;
-   }
+function checkSpaceValue(space, id) {
+  space.innerText = newGame.playerStar.token;
+  if (newGame.gameArray.includes(id)) {
+    space.innerText = '';
+  } else if (newGame.heartMoves.includes(id)) {
+    space.innerText = newGame.playerHeart.token;
+  } else if (newGame.starMoves.includes(id)) {
+    space.innerText = newGame.playerStar.token;
+  }
  }
 
 function updateReferee() {
-  if(newGame.heartTurn && !newGame.heartWinDeclared && !newGame.starWinDeclared) {
+  if (newGame.heartTurn && !newGame.heartWinDeclared && !newGame.starWinDeclared) {
     referee.innerText = `It's ${newGame.playerStar.token}'s turn`;
-  } else if(newGame.starTurn && !newGame.heartWinDeclared && !newGame.starWinDeclared) {
+  } else if (newGame.starTurn && !newGame.heartWinDeclared && !newGame.starWinDeclared) {
     referee.innerText = `It's ${newGame.playerHeart.token}'s turn`;
   }
 }
 
 function declareResult() {
   if (newGame.heartWinDeclared) {
-   referee.innerText = `${newGame.playerHeart.token} won!`;
-   newGame.resetGame();
+    referee.innerText = `${newGame.playerHeart.token} won!`;
+    newGame.resetGame();
   } else if (newGame.starWinDeclared) {
-     referee.innerText = `${newGame.playerStar.token} won!`;
-     newGame.resetGame();
-  } else if (newGame.gameArray.length === 0 && newGame.heartTurn){
+    referee.innerText = `${newGame.playerStar.token} won!`;
     newGame.resetGame();
-    referee.innerText = "It's a draw! ❤️, your turn." ;
-  } else if (newGame.gameArray.length === 0 && newGame.starTurn){
+  } else if (newGame.gameArray.length === 0) {
     newGame.resetGame();
-    referee.innerText = "It's a draw! ⭐️, your turn.";
-}
+    referee.innerText = 'It\'s a draw!';
+  }
 
 }
